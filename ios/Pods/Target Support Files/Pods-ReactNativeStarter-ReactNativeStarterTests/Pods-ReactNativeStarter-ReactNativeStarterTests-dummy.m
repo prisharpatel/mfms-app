0070b4edc578ae404496b1e44cd892cf3d6cf21f
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-12-24 12:36:16 -0800

    Delete ios/Pods directory

--- ios/Pods/Target Support Files/Pods-ReactNativeStarter-ReactNativeStarterTests/Pods-ReactNativeStarter-ReactNativeStarterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeStarter_ReactNativeStarterTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeStarter_ReactNativeStarterTests
-@end
