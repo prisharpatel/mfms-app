926b09c84a98670c8816f35b0d0f947b56277339
Julia Velloso <jvelloso@umich.edu>
2025-03-04 11:51:39 +0100

    add speaker details

--- src/modules/speakers/SpeakerDetails.js
+++ src/modules/speakers/SpeakerDetails.js
@@ -1,40 +1,61 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import {colors, fonts} from '../../styles'
+import { colors, fonts } from '../../styles';
 
 const SpeakerDetails = ({ route }) => {
   const navigation = useNavigation();
   const { speaker } = route.params; // Get speaker data
 
   return (
-    <View style={styles.container}>
-
+    <ScrollView style={styles.container}>
       {/* Speaker Details */}
-      <Image source={speaker.image} style={styles.image} />
+      <View style={styles.imageContainer}>
+        <Image
+          source={speaker.image}
+          style={styles.image}
+          resizeMode='contain' // Ensures the image is cropped from the bottom
+        />
+      </View>
       <Text style={styles.name}>{speaker.name}</Text>
+      <Text style={styles.title}>{speaker.title}</Text>
       <Text style={styles.description}>{speaker.description}</Text>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    //justifyContent: 'center',
     padding: 20,
     backgroundColor: colors.white,
   },
-  image: {
-    width: 200,
-    height: 200,
-    borderRadius: 10,
+  imageContainer: {
+    width: 250,
+    height: 250,
+    overflow: 'hidden', // Crop the bottom of the image
     marginBottom: 20,
+    borderRadius: 10,
+    alignItems: 'center', // Horizontally center the image
+    justifyContent: 'center', // Vertically center the image
+    //marginLeft: 55,
+    alignSelf: 'center'
+  },
+  image: {
+    width: '100%',
+    height: '100%', // Increase the height to crop from the bottom
+    //position: 'relative', // Allow positioning adjustments
+    //top: '15%', // Shift the image up so the bottom gets cropped
   },
   name: {
     fontSize: 24,
     fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   description: {
     fontSize: 16,
